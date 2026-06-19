39e9e71163dbe8344d4a4651b6866ffb419ffb01
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-06-19 08:26:57 -0300

    feat: register Swagger UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 import { env } from './config/env.js';
+import { openApiDocument } from './docs/openapi.js';
 import { registerRoutes } from './routes/index.js';
 
 export async function buildApp() {
@@ -10,6 +13,21 @@ export async function buildApp() {
 
   await app.register(cors, {
     origin: env.CORS_ORIGIN === '*' ? true : env.CORS_ORIGIN,
+  });
+
+  await app.register(swagger, {
+    mode: 'static',
+    specification: {
+      document: openApiDocument,
+    },
+  });
+
+  await app.register(swaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: true,
+    },
   });
 
   await registerRoutes(app);
